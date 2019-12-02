2d6163afd007512d9e7591406376012bf8d3e923
Vasiliy Tolstov <v.tolstov@unistack.org>
2019-12-03 02:23:42 +0300

    provide setServerSubscriberOption to brokers
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- context.go
+++ context.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/micro/go-micro/broker"
+	"github.com/micro/go-micro/server"
 )
 
 // setSubscribeOption returns a function to setup a context with given value
@@ -19,6 +20,16 @@ func setSubscribeOption(k, v interface{}) broker.SubscribeOption {
 // setBrokerOption returns a function to setup a context with given value
 func setBrokerOption(k, v interface{}) broker.Option {
 	return func(o *broker.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, k, v)
+	}
+}
+
+// setBrokerOption returns a function to setup a context with given value
+func setServerSubscriberOption(k, v interface{}) server.SubscriberOption {
+	return func(o *server.SubscriberOptions) {
 		if o.Context == nil {
 			o.Context = context.Background()
 		}
